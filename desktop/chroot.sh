43442dad938be1620d30b860d8ed04c97906eda8
imdabossou <jamie.lfths@hotmail.com>
2024-09-01 19:51:32 +0100

    Update chroot.sh

--- desktop/chroot.sh
+++ desktop/chroot.sh
@@ -15,3 +15,15 @@ sudo systemctl enable libvirtd
 sudo systemctl enable dnsmasq
 # Printer services
 sudo systemctl enable cups.service
+# Download and apply KDE Plasma theme
+curl -O https://github.com/imdabossou/archinstallconfig/tree/main/desktop/konsave.knsv
+konsave -i konsave.knsv
+konsave -a konsave
+# Download monitor ICM profile while still in /home
+curl -O https://github.com/imdabossou/archinstallconfig/blob/main/desktop/EX240N.icm
+# Apply Nvidia fixes
+cd /etc/modprobe.d/
+sudo curl -O https://raw.githubusercontent.com/imdabossou/archinstallconfig/main/desktop/nvidiadesktop.conf
+# Apply SDDM Wayland
+cd /etc/sddm.conf.d/
+sudo curl -O https://raw.githubusercontent.com/imdabossou/archinstallconfig/main/desktop/10-wayland.conf
