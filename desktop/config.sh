b1495d144e8b2aabdadfe7d59bc70315be8e0719
imdabossou <jamie.lfths@hotmail.com>
2024-09-03 22:31:45 +0100

    Update config.sh

--- desktop/config.sh
+++ desktop/config.sh
@@ -17,9 +17,10 @@ sudo curl -O https://raw.githubusercontent.com/imdabossou/archinstallconfig/main
 sudo mkdir /etc/sddm.conf.d/
 cd /etc/sddm.conf.d/
 sudo curl -O https://github.com/imdabossou/archinstallconfig/raw/main/desktop/10-wayland.conf
-# Download Background
+# Download Background and icon
 cd ~/Pictures
 sudo curl -O https://raw.githubusercontent.com/imdabossou/archinstallconfig/main/desktop/bg.png
+sudo curl -O https://raw.githubusercontent.com/imdabossou/archinstallconfig/main/desktop/icon.png
 # Install optional packages from Yay (separated and commented out for now for quicker testing runs of script)
 #yes | yay -S bolt-launcher orca-slicer-bin protonup-qt jamesdsp-pipewire-bin
 # Remove subvolid on btrfs mounts as it breaks restores
