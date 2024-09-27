db2dfe7cd7b59cb7da58f59b242992a93d3dec2b
imdabossou <jamie.lfths@hotmail.com>
2024-09-27 22:04:05 +0100

    Update config.sh

--- desktop/config.sh
+++ desktop/config.sh
@@ -45,5 +45,7 @@ sudo rm /etc/timeshift/default.json
 sudo curl -O https://raw.githubusercontent.com/imdabossou/archinstallconfig/main/desktop/default.json
 sudo timeshift --btrfs
 sudo systemctl enable cronie
+# AutoUpdate
+curl --silent https://raw.githubusercontent.com/cmuench/pacman-auto-update/master/install.sh | bash
 # Restart to apply changes (disabled until script completed)
 sudo reboot now
