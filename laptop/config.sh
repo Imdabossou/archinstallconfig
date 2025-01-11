d36b350cbb570e3993bedbc84799631d910ccb8d
imdabossou <jamie.lfths@hotmail.com>
2025-01-11 19:09:07 +0000

    Update config.sh

--- laptop/config.sh
+++ laptop/config.sh
@@ -3,7 +3,7 @@ cd ~
 # Install Yay
 yes | sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && yes | makepkg -si
 # Install required packages from Yay
-yes | yay -S partitionmanager mkinitcpio-firmware konsave epson-inkjet-printer-escpr2 jamesdsp
+yes | yay -S partitionmanager mkinitcpio-firmware konsave epson-inkjet-printer-escpr2
 # QEMU virtualization services
 sudo systemctl enable libvirtd
 # Printer services
@@ -20,8 +20,13 @@ cd ~/
 curl -O https://github.com/imdabossou/archinstallconfig/raw/main/laptop/arch.knsv
 konsave -i arch.knsv
 konsave -a arch
+# Bluetooth
+sudo modprobe btusb
+sudo systemctl enable bluetooth
 # Install slower packages from Yay (separated so it can be commented out for quicker testing runs of script)
 yes | yay -S bolt-launcher bambustudio-bin protonup-qt vesktop-bin zapzap
+# Bolt launcher fix
+yes | sudo pacman -S openssl-1.1 lib32-gtk2
 # Remove subvolid on btrfs mounts as it breaks restores
 sudo sed -i '/subvolid=[0-9]*/s/subvolid=[0-9]*,//g' /etc/fstab
 # Remove consolefont hook to stop error for missing config
